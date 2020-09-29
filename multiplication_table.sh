fd5a94300fe5c6bcf05a76609a82f36d5d3aa559
xmcodings <42037616+xmcodings@users.noreply.github.com>
2020-09-30 01:13:53 +0900

    multi table with shell script

---
+++ multiplication_table.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if [ $# -eq 0 ] # test if input supplied
+then 
+    echo "variable must be supplied as positive natural number"
+    exit 1
+fi
+
+if ! [ "$1" -eq "$1" ] 2> /dev/null # check if input is integer 
+then
+    echo "variale must be natual number"
+    exit 1
+fi
+
+if ! [ "$2" -eq "$2" ] 2> /dev/null #check if second input is integer
+then
+    echo "variale must be natual number"
+    exit 1
+fi
+
+
+
+    
+if [ $1 -le 0 ] # test for negative
+then 
+    echo "variables must be over 0"
+    exit 1
+fi
+
+if [ $2 -le 0 ] # test for negative
+then
+    echo "variables must be over 0"
+    exit 1
+fi
+
+for i in $(seq 1 $1)
+do
+	for j in $(seq 1 $2)
+	do
+	    ans=`expr $i \* $j`
+       	    printf "$i"x"$j=$ans"
+	    printf " "
+	done
+	echo ""
+done	
+
+
+exit 0
